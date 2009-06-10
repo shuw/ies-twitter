b32ada89747e4f00f204fcc487ae28b7e9bfc973
Shu Wu <shuwu83@gmail.com>
2009-06-10 07:54:17 +0000

    git-svn-id: https://iestwitter.googlecode.com/svn/trunk@33 33ae67a2-4314-11de-9a54-b9394989f659

--- src/edu/shu/nlt/ontology/ontotech/OntoCrunchbase.java
+++ src/edu/shu/nlt/ontology/ontotech/OntoCrunchbase.java
@@ -1,25 +1,10 @@
 package edu.shu.nlt.ontology.ontotech;
 
 import java.io.File;
-import java.net.URI;
 
-import org.semanticweb.owl.apibinding.OWLManager;
-import org.semanticweb.owl.io.RDFXMLOntologyFormat;
-import org.semanticweb.owl.model.AddAxiom;
-import org.semanticweb.owl.model.OWLClass;
-import org.semanticweb.owl.model.OWLClassAssertionAxiom;
-import org.semanticweb.owl.model.OWLDataFactory;
-import org.semanticweb.owl.model.OWLDataProperty;
-import org.semanticweb.owl.model.OWLDataPropertyAssertionAxiom;
 import org.semanticweb.owl.model.OWLIndividual;
-import org.semanticweb.owl.model.OWLObjectProperty;
-import org.semanticweb.owl.model.OWLObjectPropertyAssertionAxiom;
-import org.semanticweb.owl.model.OWLOntology;
 import org.semanticweb.owl.model.OWLOntologyChangeException;
 import org.semanticweb.owl.model.OWLOntologyCreationException;
-import org.semanticweb.owl.model.OWLOntologyManager;
-import org.semanticweb.owl.model.OWLOntologyStorageException;
-import org.semanticweb.owl.model.UnknownOWLOntologyException;
 
 import edu.shu.nlt.crunchbase.Crunchbase;
 import edu.shu.nlt.crunchbase.data.base.Company;
@@ -34,7 +19,7 @@ import edu.shu.nlt.crunchbase.data.lists.CompanyList;
  * @author shu
  * 
  */
-public class OntoCrunchbase implements Runnable {
+public class OntoCrunchbase extends OntoCreater implements Runnable {
 
 	private static final int c_minCompanyEmployees = 10;
 
@@ -49,64 +34,11 @@ public class OntoCrunchbase implements Runnable {
 				.run();
 	}
 
-	private URI base;
-
 	private CompanyList companyList;
 
-	private OWLDataFactory dataFactory;
-
-	private OWLOntologyManager manager;
-
-	private OWLOntology ontology;
-
-	private File outputFile;
-
 	public OntoCrunchbase(File input, File output) {
-		super();
-
-		this.outputFile = output;
-		manager = OWLManager.createOWLOntologyManager();
-
-		try {
-			initialize(manager, input);
-		} catch (OWLOntologyCreationException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private void assertProperty(OWLIndividual source, String predicateName, OWLIndividual target)
-			throws OWLOntologyChangeException {
-
-		OWLObjectProperty predicate = dataFactory.getOWLObjectProperty(URI.create(base + "#" + predicateName));
-
-		OWLObjectPropertyAssertionAxiom newAxiom = dataFactory.getOWLObjectPropertyAssertionAxiom(source, predicate,
-				target);
-
-		manager.applyChange(new AddAxiom(ontology, newAxiom));
-	}
-
-	private OWLIndividual createIndividual(String type, String crunchbaseId) throws OWLOntologyChangeException {
-		OWLIndividual newIndividual = dataFactory.getOWLIndividual(URI.create(base + "/crunchbase/" + type + "#"
-				+ crunchbaseId));
-
-		// Declare crunchbase Id
-		{
-			OWLDataProperty hasCrunchbaseId = dataFactory.getOWLDataProperty(URI.create(base + "#hasCrunchbaseId"));
-
-			OWLDataPropertyAssertionAxiom idAssertion = dataFactory.getOWLDataPropertyAssertionAxiom(newIndividual,
-					hasCrunchbaseId, crunchbaseId);
-
-			manager.applyChange(new AddAxiom(ontology, idAssertion));
-		}
-
-		// Decalre type of
-		{
-			OWLClass companyClass = dataFactory.getOWLClass(URI.create(base + "#" + type));
-			OWLClassAssertionAxiom isClassOfAxiom = dataFactory.getOWLClassAssertionAxiom(newIndividual, companyClass);
-			manager.applyChange(new AddAxiom(ontology, isClassOfAxiom));
-		}
-
-		return newIndividual;
+		super(input, output);
+		this.companyList = Crunchbase.getInstance().getCompanyList();
 	}
 
 	private void populateInstances() throws OWLOntologyChangeException {
@@ -138,18 +70,6 @@ public class OntoCrunchbase implements Runnable {
 				}
 			}
 		}
-	}
-
-	private void save() throws UnknownOWLOntologyException, OWLOntologyStorageException {
-		manager.saveOntology(ontology, new RDFXMLOntologyFormat(), (outputFile).toURI());
-	}
-
-	public void initialize(OWLOntologyManager manager, File inputFile) throws OWLOntologyCreationException {
-		this.manager = manager;
-		this.dataFactory = manager.getOWLDataFactory();
-		this.companyList = Crunchbase.getInstance().getCompanyList();
-		this.ontology = manager.loadOntologyFromPhysicalURI(inputFile.toURI());
-		this.base = ontology.getURI();
 	}
 
 	@Override
